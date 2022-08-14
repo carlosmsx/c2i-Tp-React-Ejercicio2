2fe08b61090f9507ac19be8df008282de683829a
carlosmsx <carlosmsx@gmail.com>
2022-08-14 02:16:37 -0300

    agregado objeto props en componente Hello

---
+++ src/components/Hello.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Hello = (props) => {
+    return (
+        <div>
+            <h1>Hello {props.nombre}!</h1>
+        </div>
+    );
+};
+
+export default Hello;
